8aef557fcf582f54645ae64fe6f5f3cdc88155c1
mrrobotxrp <urustemovsayan25@gmail.com>
2024-07-05 13:03:49 +0500

    add button to login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 useHead({
-  title: "Login",
+  title: "Login | CRM System",
+});
+
+const nameRef = ref("");
+const emailRef = ref("");
+const passwordRef = ref("");
+
+watch(emailRef, () => {
+  console.log(emailRef.value);
 });
 </script>
 
@@ -9,7 +17,29 @@ useHead({
     <div class="rounded bg-sidebar w-1/4 p-5">
       <h1 class="font-bold text-2xl text-center mb-5">Login</h1>
       <form>
-        <UiInput type="text" placeholder="shadcn" />
+        <UiInput
+          type="name"
+          placeholder="Name"
+          class="mb-3"
+          v-model="nameRef"
+        />
+        <UiInput
+          type="email"
+          placeholder="Email"
+          class="mb-3"
+          v-model="emailRef"
+        />
+        <UiInput
+          type="password"
+          placeholder="password"
+          class="mb-3"
+          v-model="passwordRef"
+        />
+
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
       </form>
     </div>
   </div>
